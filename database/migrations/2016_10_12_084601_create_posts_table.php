220d93040796105a321edade784c490f4cf34b48
programmer <clave.arnel@gmail.com>
2016-10-12 17:03:39 +0800

    update post migration

--- database/migrations/2016_10_12_084601_create_posts_table.php
+++ database/migrations/2016_10_12_084601_create_posts_table.php
@@ -14,6 +14,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
